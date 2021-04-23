e8f56888675dfc296e04a56cecfcbc7a4930a51c
DESKTOP-PF6LLID\오후F <hktrollgames@gmail.com>
2021-04-23 21:47:11 +0900

    루비 프로젝트 최초 등록

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        transform.position = position;
+    }
+}
