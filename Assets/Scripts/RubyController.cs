310cbd223aa5f24efdcc0aa7e3ed89c68ff4a502
hktrollgames <82859113+hktrollgames@users.noreply.github.com>
2021-04-25 14:07:44 +0900

    git 테스트중

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -10,11 +10,11 @@ public class RubyController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    // 강사는 다른 설명을 했다. 그래서 충돌 날것이다.
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        Debug.Log(horizontal + "그리고 다른 부분을 추가 수정했다 - git test중");
 
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
