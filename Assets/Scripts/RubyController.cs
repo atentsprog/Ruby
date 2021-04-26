31cf3759aefc26fb427e35bc607dd3820efe8dbc
hktrollgames <hktrollgames@gmail.com>
2021-04-26 19:24:42 +0900

    수직이동 추가

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -14,11 +14,42 @@ public class RubyController : MonoBehaviour
     // 화면 갱신될때마다 호출됨 - git테스트중
     void Update()
     {
+        //float horizontal = 0;
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    horizontal = -1;
+        //}
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    horizontal = 1;
+        //}
+
+        //float vertical = 0;
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    vertical = 1;
+        //}
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    vertical = -1;
+        //}
+
+        //Debug.Log(horizontal + "그리고 다른 부분을 추가 수정했다 - git test중");
+
+        //Vector2 position = transform.position;
+        //position.x = position.x + 0.1f * horizontal;
+        //position.y = position.y + 0.1f * vertical;
+        //transform.position = position;
+
+
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal + "그리고 다른 부분을 추가 수정했다 - git test중");
+        float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
 }
