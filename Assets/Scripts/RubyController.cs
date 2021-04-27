b271cbfa4390cb9ae235bb153dfe7af12a0fd161
hktrollgames <hktrollgames@gmail.com>
2021-04-27 18:19:13 +0900

    월드 인터렉션 이동제한

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start는 처음 시작되는 부분 - Git테스트중
-    // 가사가 다시 수정
+    Rigidbody2D rigidbody2d;
+
+
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // 화면 갱신될때마다 호출됨 - git테스트중
@@ -45,9 +47,11 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        Vector2 position = rigidbody2d.position;
+        position.x += 3.0f * horizontal * Time.deltaTime;
+        position.y += 3.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
     }
+    //public float speed = 3.0;
 }
